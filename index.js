cecfc3b16a6b2188895df389db6fc92b1557014c
Khushal0128 <chahodiyakhushal@gmail.com>
2025-07-29 13:28:36 +0530

    add get route

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const UserRouter = require("./routes/client");
 const HotelOwnerRouter = require("./routes/HotelOwner");
 const PORT = process.env.PORT;
 
+
+app.get("/",(req,res)=>{
+    res.json({message:"hello api"});
+})
+
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser(process.env.COOKIEPASS));
